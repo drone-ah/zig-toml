9e5ab831e55e642e925f425a8630f3f6d545fcbb
Shriram Shrikumar <shri@verks.co>
2025-06-15 09:42:05 +0100

    fix calling of serialization

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const table = @import("./table.zig");
 const struct_mapping = @import("./struct_mapping.zig");
 const datetime = @import("./datetime.zig");
 const value = @import("./value.zig");
-const serialize = @import("serialize/root.zig");
+const serialize = @import("serialize/root.zig").serialize;
 
 pub const Table = table.Table;
 pub const Date = datetime.Date;
@@ -14,7 +14,6 @@ pub const DateTime = datetime.DateTime;
 pub const Value = value.Value;
 pub const ValueList = value.ValueList;
 pub const HashMap = struct_mapping.HashMap;
-pub const tomlize = serialize.tomlize;
 
 pub const Position = parser.Position;
 pub const FieldPath = []const []const u8;
